64a804dd0cbdead038496f3ad70171dd6f0315d9
Nicholas Hansen <NicholasPHansen@users.noreply.github.com>
2019-04-23 14:42:20 +0200

    Fix fedora packagename

--- setup_script.sh
+++ setup_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 # Install the virtualenv wrapper
-python -mplatform | grep -qi Ubuntu && sudo apt install virtualenvwrapper || sudo dnf install virtualenvwrapper
+python -mplatform | grep -qi Ubuntu && sudo apt install virtualenvwrapper || sudo dnf install python3-virtualenvwrapper
 
 # Add the virtualenvwrapper script to the .bashrc file
 echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc
